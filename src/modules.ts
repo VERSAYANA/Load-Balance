9e43602a03859121f2dee1629f0bc099dc64a0b7
Ramtin Khalatbari <ramtin.khalatbarii@trialblaze.com>
2020-01-18 01:08:21 +0330

    Add fuzzification function & type

--- src/modules.ts
+++ src/modules.ts
@@ -6,7 +6,8 @@ export interface Server {
     ramUtilization: number,
     cpuUtilization: number,
     hardUtilization: number,
-    load: number
+    load: number,
+    fuzzyLoad: fuzzyLoad
 }
 
 export interface Client {
@@ -20,7 +21,10 @@ export function addClient(client: Client, server: Server) {
     return server
 }
 
- export function calUtilization(server: Server) {
+type fuzzyLoad = [number, number, number, number, number]
+
+
+export function calUtilization(server: Server): Server {
     let totalRamUsage: number = 0;
     let totalCpuUsage: number = 0;
     let totalHardUsage: number = 0;
@@ -37,11 +41,44 @@ export function addClient(client: Client, server: Server) {
     return server
 }
 
-export function calServerLoad(server: Server) {
+export function calServerLoad(server: Server): Server {
     if (server.hardUtilization >= 100) {
         server.load = 100;
     } else {
         server.load = Math.max(server.cpuUtilization, server.ramUtilization)
     }
     return server
+}
+
+export function fuzzifyServer (inputs: Array<Server>): Array<Server> {
+    for (let i = 0; i < inputs.length; i++) {
+        let load = inputs[i].load;
+        let veryLow = 0;
+        let low = 0;
+        let medium = 0;
+        let high = 0;
+        let veryHigh = 0;
+
+        if (load <= 10) {
+            veryLow = 1;
+        } else if (load > 10 && load <= 30) {
+            veryLow = (30 - load) / 20;
+            low = (load - 10) / 20;
+        } else if (load > 30 && load <= 50) {
+            low = (50 - load) / 20;
+            medium = (load - 30) / 20;
+        } else if (load > 50 && load <= 70) {
+            medium = (70 - load) / 20;
+            high = (load - 50) / 20;
+        } else if (load > 70 && load <= 90) {
+            high = (90 - load) / 20;
+            veryHigh = (load - 70) / 20;
+        } else if (load > 90) {
+            veryHigh = 1;
+        }
+
+        inputs[i].fuzzyLoad = [veryLow, low, medium, high, veryHigh];
+    }
+
+    return inputs;
 }
